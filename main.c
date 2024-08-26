75128ac77424cf561ca59aa384c6af0b925311dd
BotNikos <boss.nikitos1212@mail.ru>
2024-08-26 19:03:19 +0300

    Main hero bug-fix

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main (void) {
     SDL_Window *window = SDL_CreateWindow ("test?", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
     SDL_Surface *winSurf = SDL_GetWindowSurface (window);
 
-    SDL_Surface *image = IMG_Load ("./sprites/allSpritesUpscale-Sheet1.png");
+    SDL_Surface *image = IMG_Load ("./sprites/mainHero.png");
 
     bool running = true;
 
@@ -38,7 +38,6 @@ int main (void) {
             if (event.key.keysym.sym == SDLK_UP)
                 dest.y -= 10;
         }
-
 
         SDL_FillRect (winSurf, NULL, SDL_MapRGB (winSurf -> format, 0, 200, 50));
         SDL_BlitSurface (image, NULL, winSurf, &dest);
